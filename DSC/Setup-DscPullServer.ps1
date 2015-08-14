e5b62f158e4a98e98bc57de84789cc62de378db0
abswaxing <alex.smart@outlook.com>
2015-08-14 16:31:18 -0500

    Minor update, please ignore!

--- DSC/Setup-DscPullServer.ps1
+++ DSC/Setup-DscPullServer.ps1
@@ -1,6 +1,7 @@
 ﻿
 # Create certificate to encrypt and decrypt credentials in the MOF configuration files.
 
+
 $DnsName = "Dsc Credential Certificate 2"
 $PfxPassword = ConvertTo-SecureString -String "SUPERCOMPLEXEPASSWORDHERE" -Force -AsPlainText
 $PrivateOutPath = "E:\temp\PrivateDscCertificate.pfx"
@@ -10,6 +11,7 @@ New-SelfSignedCertificate -DnsName $DnsName -CertStoreLocation Cert:\LocalMachin
 $Certificate = Get-ChildItem -Path Cert:\LocalMachine\My | Where {$_.Subject -match $DnsName }
 Export-PfxCertificate -Cert $Certificate -FilePath $PrivateOutPath -Password $PfxPassword
 Export-Certificate -Cert $Certificate -FilePath $PublicOutPath
+
 
 
 # Pull Server configuration
