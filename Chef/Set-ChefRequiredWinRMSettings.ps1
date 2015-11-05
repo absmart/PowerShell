9438806ca5cd4d6c63cb0bf6b6856239d1b83a9a
abswaxing <alex.smart@outlook.com>
2015-11-05 15:58:20 -0600

    Removed unencrypted winrm config

--- Chef/Set-ChefRequiredWinRMSettings.ps1
+++ Chef/Set-ChefRequiredWinRMSettings.ps1
@@ -1,7 +1,7 @@
 ﻿winrm quickconfig -q
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
  
 netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
