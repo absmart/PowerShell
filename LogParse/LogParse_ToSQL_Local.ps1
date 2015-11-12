167bfe3fe3a89e51c9c5250015c1f810d2178bff
abswaxing <alex.smart@outlook.com>
2015-11-12 13:11:14 -0600

    New logparse script!

---
+++ LogParse/LogParse_ToSQL_Local.ps1
@@ -0,0 +1,56 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+	[Parameter(Mandatory=$true)]
+	[string] $ConfigFile
+)
+
+$Config = [xml] ( Get-Content $ConfigFile )
+
+function Get-IISFileName 
+{			
+	param(
+		[string] $LogFormat,
+	    [ValidateSet("Daily","Monthly")] $Range = "Daily"
+	)
+
+    Switch ($Range){
+        "Daily" {
+            return $(Get-Date).AddDays(-1).ToString($LogFormat)  + ".log"    
+        }
+        "Monthly" {
+            return $(Get-Date).AddMonths(-1).ToString($LogFormat)  + "*.log"
+        }
+    }
+}
+
+$Servers += @($Config.logparse.servers.server)
+$Sites += @($Config.logparse.sites.site)
+$Queries += @( $Config.logparse.queries.query | ? { -not [String]::IsNullOrEmpty($_.sql) } )
+$LogPath = $Config.logparse.log_path
+
+$LogParse = "D:\Utils\logparser.exe"
+
+$LogName = Get-IISFileName -LogFormat $Config.logparse.fileformat -Range $Config.logparse.logging_range
+Write-Verbose ("Log Name - " + $Log_Name)
+
+foreach( $Site in $Sites )
+{
+	if( $Site.id -eq $null ){
+		$id = "W3SVC" + (Get-Website | where { $_.Name -imatch $Site.Name } | Select -First 1 -Expand id).ToString()
+	}
+	else {
+		$id = $Site.id
+	}
+
+	$Log =  $LogPath + "\" + $id + "\" + $LogName
+				
+	if( Test-Path $Log ) 
+	{
+		foreach($Query in $Queries)
+		{
+            $Sql = ($Query.sql -f $Log) + " " + $Query.option
+            Write-Verbose "Sql variable : $Sql"
+			&$LOGPARSE $sql
+		}
+	}
+}
