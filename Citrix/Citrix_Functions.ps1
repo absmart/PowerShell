d9fadcd9380cf7c59a668915a31abc2f01d9cc86
abswaxing <alex.smart@outlook.com>
2015-11-25 11:12:20 -0600

    Consolidating scripts into Functions module

--- Citrix/Citrix_Functions.ps1
+++ Citrix/Citrix_Functions.ps1
@@ -1,13 +1,13 @@
-﻿Import-Module (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\PvsPSSnapin.dll")
-Import-Module (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\Citrix.Common.Commands.Data.dll")
-Import-Module (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\Citrix.Common.Commands.dll")
-Import-Module (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\BrokerSnapin.dll")
-Import-Module (Join-Path $env:POWERSHELL_HOME "Libraries\General_Variables.psm1")
+﻿Import-Module -Name (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\PvsPSSnapin.dll")
+Import-Module -Name (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\Citrix.Common.Commands.Data.dll")
+Import-Module -Name (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\Citrix.Common.Commands.dll")
+Import-Module -Name (Join-PATH $env:POWERSHELL_HOME "Citrix\Modules\BrokerSnapin.dll")
+Import-Module -Name (Join-Path $env:POWERSHELL_HOME "Libraries\General_Variables.psm1")
 
 function Get-XAAppReport
 {
 	param(
-		[string] $AppName,
+		[string] $ApplicationName,
         [ValidateSet("Equals","Matches")] [string] $Match,
 		[string] $DataCollector = $citrix_environment.Farm01.DATA_COLLECTOR
 	)
@@ -16,12 +16,12 @@ function Get-XAAppReport
 			$AppName,
             $Match
 		)
-		Add-PSSnapin Citrix.XenApp.Commands
-
+		Add-PSSnapin -Name Citrix.XenApp.Commands        
+        $Result = $null        
         switch ($Match)
         {
-            "Equals"  { $Result = Get-XAApplication | Where {$_.BrowserName -eq $AppName} | Get-XAApplicationReport }
-            "Matches" { $Result = Get-XAApplication | Where {$_.BrowserName -match $AppName} | Get-XAApplicationReport }
+            "Equals"  { $Result = Get-XAApplication | Where-Object {$_.BrowserName -eq $AppName} | Get-XAApplicationReport }
+            "Matches" { $Result = Get-XAApplication | Where-Object {$_.BrowserName -match $AppName} | Get-XAApplicationReport }
         }
         return $Result
 	}
@@ -36,13 +36,13 @@ function Get-XAServers
         Add-PSSnapin Citrix.XenApp.Commands
         Get-XAServer
     }
-    return $Servers | Sort -Property ServerName | Select ServerName 
+    return $Servers | Sort-Object -Property ServerName | Select-Object ServerName 
 }
 
 function Get-XAServerLoadByEnv
 {
     param(
-		[ValidateSet("7.6","6.0")] [string] $CitrixEnvironment
+		[ValidateSet("7.6","6.0","4.5.1")] [string] $CitrixEnvironment
     )
 	switch ($CitrixEnvironment)
 	{
@@ -58,7 +58,13 @@ function Get-XAServerLoadByEnv
 				$Results = Get-XAServerLoad | Select @{Name="XAServer";Expression={$_.ServerName}},@{Name="Date";Expression={Get-Date}},@{Name="ServerLoad";Expression={$_.Load}},@{Name="ServerLoadPercent";Expression={($_.Load/10000)*100}} | Sort-Object XAServer
 				return $Results
             }
-		}
+        }
+        "4.5.1" {
+            [string] $XAServer = $citrix_environment.Farm03.DATA_COLLECTOR
+            Invoke-Command -ComputerName $XAServer -ScriptBlock{
+                qfarm /load
+            }                
+        }
     }
 }
 Set-Alias -Name XALoad -Value Get-XAServerLoadByEnv
@@ -108,7 +114,7 @@ function Get-XAApplicationUsage
         foreach($Server in $Servers){
             Get-XASessionProcess -ServerName $Server | 
                 Where {$_.ProcessName -match $ProcessName} |
-                Select @{Name="Date";Expression={Get-Date -Format s}}, AccountDisplayName, ProcessName, SessionId, ProcessId, ServerName, CreationTime, PercentCPULoad, @{Name="Memory (in MB)";Expression={($_.CurrentWorkingSetSize/1mb)}} | FT
+                Select-Object @{Name="Date";Expression={Get-Date -Format s}}, AccountDisplayName, ProcessName, SessionId, ProcessId, ServerName, CreationTime, PercentCPULoad, @{Name="Memory (in MB)";Expression={($_.CurrentWorkingSetSize/1mb)}} | Format-Table
         }
     }
 } 
@@ -125,11 +131,11 @@ function Get-XAAssignedUsersForApplication
 				[string] $Application
 			)
 		
-			Add-PSSnapIn Citrix.Common.Commands -EA SilentlyContinue
-			Add-PSSnapin Citrix.XenApp.Commands -EA SilentlyContinue
-		
+			#Add-PSSnapIn -Name Citrix.Common.Commands -ErrorAction SilentlyContinue
+			Add-PSSnapin -Name Citrix.XenApp.Commands -ErrorAction SilentlyContinue
+
 			$Details = Get-XAApplication $Application | Get-XAAccount | Select AccountDisplayName, AccountType, SearchPath
-		
+
 			return $Details
 		}
 }
@@ -140,7 +146,8 @@ function Get-XASession
     param(
         [string] $UserName,
         [string] $ComputerName = $citrix_environment.Farm01.DATA_COLLECTOR,
-        [string] $UserDomain = $citrix_environment.Farm01.Domain
+        [string] $UserDomain = $citrix_environment.Farm01.Domain,
+        [string] $Application = $null
     )
 
     Invoke-Command -ComputerName $ComputerName -ArgumentList $UserName, $UserDomain -ScriptBlock {
@@ -149,7 +156,7 @@ function Get-XASession
             [string] $UserDomain
         )
 
-        Add-PSSnapin Citrix.XenApp.Commands
+        Add-PSSnapin -Name Citrix.XenApp.Commands
 
         if($UserName -imatch $UserDomain)
         {
@@ -162,7 +169,7 @@ function Get-XASession
             $Sessions = Get-XASession -Account $Account
         }
 
-        return $Sessions | ft
+        return $Sessions | Format-Table
     }
 }
 
@@ -180,7 +187,7 @@ function Stop-XASession
             [string] $UserDomain
         )
 
-        Add-PSSnapin Citrix.XenApp.Commands
+        Add-PSSnapin -Name Citrix.XenApp.Commands
 
         if($UserName -imatch $UserDomain)
         {
@@ -196,13 +203,61 @@ function Stop-XASession
         try
         {
             $Sessions | Stop-XASession
-            Write-Host "The following sessions have been stopped." -ForegroundColor Green
-            return $Sessions | ft
+            Write-Output "The following sessions have been stopped." -ForegroundColor Green
+            return $Sessions | Format-Table
         }
         catch
         {
-            Write-Host "No sessions found for $Account." -ForegroundColor Yellow
-            return $Sessions | ft
+            Write-Output "No sessions found for $Account." -ForegroundColor Yellow
+            return $Sessions | Format-Table
         }
     }
+}
+
+function Restart-IMAService
+{
+    param(
+        [string] $ComputerName = "localhost"
+        
+        if($Computer -eq "localhost"){
+            Restart-Service -Name IMAService -Force -Verbose
+        }
+        else{
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock{
+                Restart-Service -Name IMAService -Force -Verbose
+            }
+        }
+    )
+}
+
+function Set-XAServiceRecovery{
+    param(
+        $ComputerName
+    )
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock{
+        sc.exe failure cpsvc reset= 86400 actions= restart/5000/restart/5000/restart/5000
+        sc.exe failure Imaservice reset= 86400 actions= restart/5000/restart/5000/restart/5000
+    }
+}
+
+function Get-XAWindowsEvent{
+    param(
+        $ComputerName
+        [System.Int32] $Days = "1"
+    )
+    $YDate = (Get-Date.AddDays(-$Days))    
+    $Results += Invoke-Command -ComputerName $XAServers -ArgumentList $YDate -ScriptBlock{
+        param(
+            $YDate
+        )    
+        $TDate = (Get-Date)        
+        $Events += Get-EventLog -LogName System -Source IMAService -After $YDate -Before $TDate -EntryType Warning -ErrorAction SilentlyContinue
+        $Events += Get-EventLog -LogName System -Source IMAService -After $YDate -Before $TDate -EntryType Error -ErrorAction SilentlyContinue
+        $Events += Get-EventLog -LogName System -Source CitrixHealthMon -After $YDate -Before $TDate -EntryType Warning -ErrorAction SilentlyContinue
+        $Events += Get-EventLog -LogName System -Source CitrixHealthMon -After $YDate -Before $TDate -EntryType Error -ErrorAction SilentlyContinue
+        $Events += Get-EventLog -LogName System -Source Metaframe -After $YDate -Before $TDate -EntryType Warning -ErrorAction SilentlyContinue
+        $Events += Get-EventLog -LogName System -Source Metaframe -After $YDate -Before $TDate -EntryType Error -ErrorAction SilentlyContinue
+        return $Events
+    }
+    return $Results
 }
