ee3e664b6a816f6baa515b4c0697caf40e4438a5
Alex Smart <alex.smart@outlook.com>
2016-02-19 11:39:10 -0600

    Azure template deploy
    Added some templates from mesos/swarm from Azure.github.com to test
    deploying templates to Azure from PowerShell.

---
+++ Azure/Deploy-AzureTemplate.ps1
@@ -0,0 +1,33 @@
+﻿param(
+    [Parameter(ParameterSetName='TemplateFile')]
+    $TemplateFile,
+    [Parameter(ParameterSetName='TemplateFile')]
+    $ParameterFile,
+
+    [Parameter(ParameterSetName='TemplateUri')]
+    $TemplateUri,
+
+    $ResourceGroup = (Read-Host "Enter the ResourceGroup: "),
+    $Location = (Read-Host "Enter the Location: "),
+    $DeploymentName
+)
+
+try{Get-AzureRmSubscription}
+catch{Login-AzureRmAccount}
+
+try{Get-AzureRMResourceGroup -Name $ResourceGroup -Location $Location}
+catch{New-AzureRMResourceGroup -Name $ResourceGroup -Location $Location}
+
+if(!($DeploymentName)){
+    $DeploymentName = $ResourceGroup + "-deployment"
+}
+
+if($TemplateUri){
+    New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $ResourceGroup -TemplateUri $TemplateUri
+}
+else{
+    New-AzureRmResourceGroupDeployment -Name $DeploymentName `
+        -ResourceGroupName $ResourceGroup `
+        -TemplateFile $TemplateFile `
+        -TemplateParameterFile $ParameterFile
+}
