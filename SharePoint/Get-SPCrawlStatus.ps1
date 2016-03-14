52ec41bba040973976915de29cf24c039b98b592
Alex Smart <alex.smart@outlook.com>
2016-03-14 14:08:06 -0500

    New SharePoint folder

---
+++ SharePoint/Get-SPCrawlStatus.ps1
@@ -0,0 +1,33 @@
+﻿param(
+    $ComputerName
+)
+
+$sources = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+    Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+    $searchApp = Get-SPServiceApplication | Where-Object {$_.DisplayName -eq "Search Service Application"}
+
+    if($searchApp -eq $null){
+        $searchApp =  Get-SPServiceApplication | where { $_.TypeName -eq "Search Service Application" }
+    }
+
+    $sources = $searchApp | Get-SPEnterpriseSearchCrawlContentSource
+    return $sources
+}
+
+foreach($source in $sources){
+
+    $status = Invoke-Command -ComputerName $ComputerName -ArgumentList $source -ScriptBlock {
+        param(
+            $source
+        )
+
+        Add-PSSnapin Microsoft.SharePoint.PowerShell
+        
+        $status = $source | Select CrawlState, CrawlStatus, DeleteCount, ErrorCount, LevelHighErrorCount, SuccessCount, FullCrawlSchedule, IncrementalCrawlSchedule, StartAddress, CrawlStarted
+
+        return $status
+    }
+
+    $status
+}
---
+++ SharePoint/Get-SPTimerStatus.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $JobName,
+    $ComputerName
+)
+
+foreach($job in $jobName){
+
+    $status = Invoke-Command -ComputerName $ComputerName -ArgumentList $JobName -ScriptBlock {
+        param(
+            $JobName
+        )
+        Add-PSSnapin Microsoft.SharePoint.PowerShell
+    
+        $return = (Get-SPTimerJob -Identity $JobName).HistoryEntries | Sort-Object -Descending -Property StartTime | Select-Object -First 1 
+        return $return
+    }
+
+    $status
+}
