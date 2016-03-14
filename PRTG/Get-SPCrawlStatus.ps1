c8728ebfb2ab753ba2c5b0c57f3ec27d81633538
Alex Smart <alex.smart@outlook.com>
2016-03-14 14:07:51 -0500

    New PRTG Scripts
    Scripts used to return data in a custom PRTG sensor.

---
+++ PRTG/Get-SPCrawlStatus.ps1
@@ -0,0 +1,57 @@
+﻿param(
+    $ComputerName,
+    $Username,
+    $Password
+)
+
+$securePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($Username, $securePassword)
+
+$sources = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+    Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+    $searchApp = Get-SPServiceApplication | Where-Object {$_.DisplayName -eq "Search Service Application"}
+
+    if($searchApp -eq $null){
+        $searchApp =  Get-SPServiceApplication | where { $_.TypeName -eq "Search Service Application" }
+    }
+
+    $sources = $searchApp | Get-SPEnterpriseSearchCrawlContentSource
+    return $sources
+}
+
+foreach($source in $sources){
+
+    $status = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ArgumentList $source -ScriptBlock {
+        param(
+            $source
+        )
+
+        Add-PSSnapin Microsoft.SharePoint.PowerShell
+        
+        $status = $source | Select CrawlState, CrawlStatus, DeleteCount, ErrorCount, LevelHighErrorCount, SuccessCount, FullCrawlSchedule, IncrementalCrawlSchedule, StartAddress, CrawlStarted
+
+        return $status
+    }
+
+    $crawlState = $status.ContentSource.CrawlSchedule
+    $crawlErrorCount = $status.ContentSource.CrawlStatus
+    $crawlLevelHighErrorCount = $status.LevelHighErrorCount
+    $fullCrawlErrorCount = $status.ContentSource.LevelHighErrorCount
+
+    # Return results to host for PRTG to capture
+
+    Write-Host "<prtg>"
+    "<result>"
+    "<channel>crawlState</channel>"
+    "<value>$crawlState</value>"
+    "</result>"
+    "</prtg>"
+
+    Write-Host "<prtg>"
+    "<result>"
+    "<channel>crawlLevelHighErrorCount</channel>"
+    "<value>$crawlLevelHighErrorCount</value>"
+    "</result>"
+    "</prtg>"
+}
---
+++ PRTG/Get-SPTimerStatus.ps1
@@ -0,0 +1,40 @@
+﻿param(
+    $JobName,
+    $ComputerName,
+    $Username,
+    $Password
+)
+
+$securePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($Username, $securePassword)
+
+foreach($job in $jobName){
+
+    $status = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ArgumentList $JobName -ScriptBlock {
+        param(
+            $JobName
+        )
+        Add-PSSnapin Microsoft.SharePoint.PowerShell
+    
+        $return = (Get-SPTimerJob -Identity $JobName).HistoryEntries | Sort-Object -Descending -Property StartTime | Select-Object -First 1 
+        return $return
+    }
+    $jobStatus = $status.Status
+    $jobErrorMessage = $status.ErrorMessage
+
+    if($jobErrorMessage -eq $null){
+        [system.string] $jobStatus = ""
+    }
+    else{
+        [system.string] $jobError = $jobErrorMessage
+    }
+
+    # Return results to host for PRTG to capture
+
+    Write-Host "<prtg>"
+    "<result>"
+    "<channel>$jobName</channel>"
+    "<value>$jobStatus</value>"
+    "</result>"
+    "</prtg>"
+}
