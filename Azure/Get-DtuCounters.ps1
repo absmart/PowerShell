ce4949ddda47a61475a3df2577b10ab188c44574
Alex Smart <alex.smart@outlook.com>
2016-06-09 10:46:36 -0500

    dtucounter
    Made my own DTU Counter script for estimating Elastic and Azure SQL
    Database sizes

---
+++ Azure/Get-DtuCounters.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+param (
+
+    [String] $DatabaseName = $null,
+    $SampleInterval = 1,
+    $MaxSamples = 3600,
+    $ExportPath = "C:\sql-perfmon-log.csv"
+)
+
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "Continue"
+
+cls
+
+if($DatabaseName -eq $null){ $DatabaseName = "_Total"; Write-Output "Collecting _Total counter for SQLServer:Databases\Log Bytes Flushed/sec"}
+Write-Output "Collecting counters..."
+Write-Output "Press Ctrl+C to exit."
+
+$counters = @("\Processor(_Total)\% Processor Time", "\LogicalDisk(C:)\Disk Reads/sec", "\LogicalDisk(C:)\Disk Writes/sec", 
+    "\LogicalDisk(C:)\Disk Read Bytes/sec", "\LogicalDisk(C:)\Disk Write Bytes/sec", "\SQLServer:Databases($DatabaseName)\Log Bytes Flushed/sec") 
+
+Get-Counter -Counter $counters -SampleInterval $SampleInterval -MaxSamples $MaxSamples | 
+    Export-Counter -FileFormat csv -Path $ExportPath -Force
