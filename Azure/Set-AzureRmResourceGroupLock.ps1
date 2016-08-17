bd8baa8e92e4540eac8a3f4bf3f11dc8106e1851
Alex Smart <alex.smart@outlook.com>
2016-08-17 15:58:30 -0500

    set locks

---
+++ Azure/Set-AzureRmResourceGroupLock.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    $resourceGroup = (Get-AzureRmResourceGroup).ResourceGroupName
+)
+
+foreach($rg in $resourceGroup){
+    New-AzureRmResourceLock -ResourceGroupName $rg -LockName CannotDelete -LockLevel CanNotDelete -Verbose -Force
+}
