872b37308430a9cbaf30fbc426860ee1944bf528
Alex Smart <alex.smart@outlook.com>
2016-08-17 15:06:45 -0500

    DC demo

---
+++ Azure/Setup-DemoDomainController.ps1
@@ -0,0 +1,22 @@
+﻿#
+# Windows PowerShell script for AD DS Deployment
+#
+Import-Module ServerManager
+
+Install-WindowsFeature -Name AD-Domain-Services
+
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "Win2012R2" `
+    -DomainName "absdomainlab2.local" `
+    -DomainNetbiosName "ABSDOMAINLAB1" `
+    -ForestMode "Win2012R2" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$false `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true
+
