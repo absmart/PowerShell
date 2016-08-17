62257acdad10a23bc7400df3efba23bef5122c6b
Alex Smart <alex.smart@outlook.com>
2016-08-17 15:05:28 -0500

    query inherited ntfs permissions

---
+++ Audit/Get-FolderInheritance.ps1
@@ -0,0 +1,29 @@
+﻿param(
+    $Path
+)
+
+$directoryChildItems = (Get-ChildItem -Path $Path -Recurse -Directory)
+$results = @()
+    
+foreach($directory in $directoryChildItems)
+{
+    #$inheritance = ($directory | Get-Acl).Access | Where-Object {$_.InheritanceFlags -eq $false}
+    $acls = (Get-Acl -Path $directory.FullName).Access
+    foreach ($acl in $acls)
+    {
+        if($acl.IsInherited -eq $false)
+        {
+            $result = [ordered]@{
+                FolderPath = $directory.FullName;
+                IsInherited = $acl.IsInherited;
+                InheritanceFlags = $acl.InheritanceFlags;
+                PropagationFlags = $acl.PropagationFlags
+            }
+
+        $results += (New-Object -TypeName PSObject -Property $result)
+            
+        }
+    }
+}
+
+return $results
