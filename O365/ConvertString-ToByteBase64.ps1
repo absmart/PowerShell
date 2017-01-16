424b4efb2b96352df92c8131e728ae56e691e464
Alex Smart <alex.smart@outlook.com>
2017-01-16 16:21:11 -0600

    Script to convert to base64 for immutableId lookup

---
+++ O365/ConvertString-ToByteBase64.ps1
@@ -0,0 +1,10 @@
+﻿$enc = [system.Text.Encoding]::UTF8
+$string1 = "DEF456" 
+$data1 = $enc.GetBytes($string1) 
+
+# Create a New SHA1 Crypto Provider 
+$sha = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider 
+
+# Now hash and display results 
+$result1 = $sha.ComputeHash($data1)
+[System.Convert]::ToBase64String($result1)
