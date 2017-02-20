a9cb0508b568cf9626bf07cf68234e257dea4367
Alex Smart <alex.smart@outlook.com>
2017-02-20 16:34:36 -0600

    add mgmt tools and add restart

--- Azure/Setup-DemoDomainController.ps1
+++ Azure/Setup-DemoDomainController.ps1
@@ -3,7 +3,7 @@
 #
 Import-Module ServerManager
 
-Install-WindowsFeature -Name AD-Domain-Services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 Import-Module ADDSDeployment
 
@@ -11,12 +11,13 @@ Install-ADDSForest `
     -CreateDnsDelegation:$false `
     -DatabasePath "C:\Windows\NTDS" `
     -DomainMode "Win2012R2" `
-    -DomainName "absdomainlab2.local" `
+    -DomainName "absdomainlab1.local" `
     -DomainNetbiosName "ABSDOMAINLAB1" `
     -ForestMode "Win2012R2" `
     -InstallDns:$true `
     -LogPath "C:\Windows\NTDS" `
     -NoRebootOnCompletion:$false `
     -SysvolPath "C:\Windows\SYSVOL" `
-    -Force:$true
+    -Force:$true 
 
+Restart-Computer
